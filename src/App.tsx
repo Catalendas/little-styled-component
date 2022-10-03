9230e24ae20351488cc5304df8b5cbd219c59fe0
MarcosAlexandre <82763928+Catalendas@users.noreply.github.com>
2022-10-02 22:35:15 -0300

    Styled-component

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Button } from "./components/Button"
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from '../styles/themes/default'
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary"/>
+      <Button variant="secundary"/>
+      <Button variant="danger"/>
+      <Button variant="success"/>
+      <Button />
+    </ThemeProvider>
+  )
+}
+
+export default App
